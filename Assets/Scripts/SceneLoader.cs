3596f139d166b1e03c1f7bc3ae56b417d6c30c43
Richard Marcoux <marcourp@morrisville.edu>
2020-02-27 13:36:36 -0500

    Button Click Loads Scene

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,15 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        // determine our present scene
+        int intCurrentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        // load next scene
+        SceneManager.LoadScene(intCurrentSceneIndex + 1);
+    }
+    
+} // SceneLoader
